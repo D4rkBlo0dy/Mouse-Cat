22fc826990058f16e4151f2cd5bbe9d9ce807bb6
thiago vazquez <thiago.vazquez@academy.local>
2026-02-10 19:51:08 -0300

    Agregue movimientos del raton

--- laberinto.py
+++ laberinto.py
@@ -40,7 +40,7 @@ def colocar_elementos(tablero):
     gato_pos = (0, 6)
     queso_pos = (6, 6)
 
-    paredes = {
+    paredes= {
         (1,1), (1,2), (1,3),
         (3,1), (3,2), (3,3),
         (4,1), (5,1)
@@ -57,14 +57,67 @@ def colocar_elementos(tablero):
 
     return raton_pos, gato_pos, queso_pos, paredes
 
+#movimientos
+
+movimientos = {
+    "w":(-1, 0),
+    "s":(1, 0),
+    "a":(0, -1),
+    "d":(0, 1)
+}
+
+#validacion de posiciones
+
+def dentro_del_mapa(pos, paredes):
+    x, y = pos
+
+    if x < 0 or x >= tamaño:
+        return False
+    elif y < 0 or y >= tamaño:
+        return False
+    elif pos in paredes:
+        return False
+    return True
+
+#mover al raton
+
+def mover_raton(tablero, raton_pos, paredes):
+
+    print("Mover con WASD:")
+    tecla = input("> ").lower()
+
+    if tecla not in movimientos:
+        return raton_pos
+    
+    dx, dy = movimientos[tecla]
+
+    nuevo = (
+        raton_pos[0] + dx,
+        raton_pos[1] + dy
+
+    )
+
+    if not dentro_del_mapa(nuevo, paredes):
+        print("movimiento invalido")
+        return raton_pos
+    
+    #limpiar vieja posicion
+    tablero[raton_pos[0]][raton_pos[1]] = vacio
+
+    #nueva posicion
+    tablero[nuevo[0]][nuevo[1]] = raton
+    return nuevo
+
 #programa principal
 
 def main():
     tablero = crear_tablero()
 
-    raton, gato, queso, pared = colocar_elementos(tablero)
+    raton_pos, gato_pos, queso_pos, paredes = colocar_elementos(tablero)
 
-    dibujar_tablero(tablero)
+    while True:
+        dibujar_tablero(tablero)
+        raton_pos = mover_raton(tablero, raton_pos, paredes)
 
 if __name__ == "__main__":
     main()
